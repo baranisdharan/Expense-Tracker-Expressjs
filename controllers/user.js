e59b2628c5635c95d983a0358dc9ebd5541acc5f
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-07-25 00:52:28 +0530

    Correction to last comit

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,4 @@
 const User = require('../models/users');
-const bcrypt = require('bcrypt');
 
 function isstringinvalid(string){
     if(string==undefined || string.length==0){
@@ -27,28 +26,20 @@ exports.postSignup = async(req,res)=>{
 
 exports.postLogin = async(req,res)=>{
     try{
-    const{email,password}=req.body;
-    console.log(password)
+    const{email,password}=req.body;    
     if(isstringinvalid(email) || isstringinvalid(password)){
         res.status(400).json({message:'email or password is missing',success:false})
     }
-    const user=await User.findAll({where:{email}})
+    const user = await User.findAll({where:{email}})
         if(user.length>0){
-            bcrypt.compare(password,user[0].password,(err,result)=>{
-                if(err){
-                    throw new Error('something went wrong')
-                }
-                if(result===true){
-                    res.status(200).json({success:true,message:'user logged in successfully',token:generateAccessToken(user[0].id,user[0].name,user[0].ispremiumuser)})
-                }else{
-                    return res.status(400).json({success:false,message:'password is incorrect'})
-                }
-            })
-
+            if(user[0].password===password){
+                res.status(200).json({success:true,message:'user logged in successfully'})
+            }else{
+                return res.status(400).json({success:false,message:'password is incorrect'})
+            }
         }else{
-            return res.status(404).json({success:false,message:'user doesnot exist'})
-        }
-    
+                return res.status(404).json({success:false,message:'user doesnot exist'})
+            }          
    } catch{err=>{
         res.status(500).json({message:err,success:false})
     }}
