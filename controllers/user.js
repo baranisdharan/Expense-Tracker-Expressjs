9db1dc400e30093e5e39b654d03e9d807010fe78
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-07-25 00:34:49 +0530

    Creating Login Screen Frontend

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
 
 function isstringinvalid(string){
     if(string==undefined || string.length==0){
@@ -21,5 +22,34 @@ exports.postSignup = async(req,res)=>{
 }
     catch{(err)=>{
         return res.status(500).json(err)
+    }}
+}
+
+exports.postLogin = async(req,res)=>{
+    try{
+    const{email,password}=req.body;
+    console.log(password)
+    if(isstringinvalid(email) || isstringinvalid(password)){
+        res.status(400).json({message:'email or password is missing',success:false})
+    }
+    const user=await User.findAll({where:{email}})
+        if(user.length>0){
+            bcrypt.compare(password,user[0].password,(err,result)=>{
+                if(err){
+                    throw new Error('something went wrong')
+                }
+                if(result===true){
+                    res.status(200).json({success:true,message:'user logged in successfully',token:generateAccessToken(user[0].id,user[0].name,user[0].ispremiumuser)})
+                }else{
+                    return res.status(400).json({success:false,message:'password is incorrect'})
+                }
+            })
+
+        }else{
+            return res.status(404).json({success:false,message:'user doesnot exist'})
+        }
+    
+   } catch{err=>{
+        res.status(500).json({message:err,success:false})
     }}
 }
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,7 @@ const router=express.Router()
 const userController=require('../controllers/user')
 
 router.post('/user/signup',userController.postSignup)
-
-
-
-
-
+router.post('/user/login',userController.postLogin)
 
 
 module.exports=router;
